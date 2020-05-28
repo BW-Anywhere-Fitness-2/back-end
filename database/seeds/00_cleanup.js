128318d90ae45b6d0d9204a26b7915ff50e4fb47
Blair <abrgroup@gmail.com>
2020-05-28 13:49:53 -0400

    testing out postgres seeding issue

--- database/seeds/00_cleanup.js
+++ database/seeds/00_cleanup.js
@@ -1,22 +1,22 @@
-exports.seed = async function(knex) {
-  if (process.env.NODE_ENV != "production") {
-    await knex("class").truncate();
-    await knex("user").truncate();
-    await knex("instructor").truncate();
-  }
-};
-
 // exports.seed = async function(knex) {
-//   switch (process.env.NODE_ENV) {
-//     case "production":
-//       await knex.raw("TRUNCATE TABLE class RESTART IDENTITY CASCADE");
-//       await knex.raw("TRUNCATE TABLE user RESTART IDENTITY CASCADE");
-//       await knex.raw("TRUNCATE TABLE instructor RESTART IDENTITY CASCADE");
-//       break;
-//     default:
-//       // anything not using Postgres
-//       await knex("class").truncate();
-//       await knex("user").truncate();
-//       await knex("instructor").truncate();
+//   if (process.env.NODE_ENV != "production") {
+//     await knex("class").truncate();
+//     await knex("user").truncate();
+//     await knex("instructor").truncate();
 //   }
 // };
+
+exports.seed = async function(knex) {
+  switch (process.env.NODE_ENV) {
+    case "production":
+      await knex.raw("TRUNCATE TABLE user CASCADE");
+      await knex.raw("TRUNCATE TABLE instructor CASCADE");
+      await knex.raw("TRUNCATE TABLE class CASCADE");
+      break;
+    default:
+      // anything not using Postgres
+      await knex("user").truncate();
+      await knex("instructor").truncate();
+      await knex("class").truncate();
+  }
+};
