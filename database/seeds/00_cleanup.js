c4d05ada1f82da8bb3faa11a49ec450e4a12d6f5
Blair <abrgroup@gmail.com>
2020-05-27 21:51:21 -0400

    seed changes for postgres

--- database/seeds/00_cleanup.js
+++ database/seeds/00_cleanup.js
@@ -1,5 +1,7 @@
 exports.seed = async function(knex) {
-  await knex("class").truncate();
-  await knex("user").truncate();
-  await knex("instructor").truncate();
+  if (process.env.NODE_ENV != "production") {
+    await knex("class").truncate();
+    await knex("user").truncate();
+    await knex("instructor").truncate();
+  }
 };
