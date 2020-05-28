aa853ac769c269aeb90128ea7f0e830dd1e50779
Blair <abrgroup@gmail.com>
2020-05-28 14:32:37 -0400

    working on seeds issue

--- database/seeds/00_cleanup.js
+++ database/seeds/00_cleanup.js
@@ -5,18 +5,17 @@
 //     await knex("instructor").truncate();
 //   }
 // };
-
-// exports.seed = async function(knex) {
-//   switch (process.env.NODE_ENV) {
-//     case "production":
-//       await knex.raw("TRUNCATE TABLE user CASCADE");
-//       await knex.raw("TRUNCATE TABLE instructor CASCADE");
-//       await knex.raw("TRUNCATE TABLE class CASCADE");
-//       break;
-//     default:
-//       // anything not using Postgres
-//       await knex("user").truncate();
-//       await knex("instructor").truncate();
-//       await knex("class").truncate();
-//   }
-// };
+exports.seed = async function(knex) {
+  //   switch (process.env.NODE_ENV) {
+  //     case 'production':
+  //       await knex.raw('TRUNCATE TABLE user CASCADE')
+  //       await knex.raw('TRUNCATE TABLE instructor CASCADE')
+  //       await knex.raw('TRUNCATE TABLE class CASCADE')
+  //       break
+  //     default:
+  //       // anything not using Postgres
+  //       await knex('user').truncate()
+  //       await knex('instructor').truncate()
+  //       await knex('class').truncate()
+  //   }
+};
