abf68c3cad7060a1e88649a721154750327759fe
Blair <abrgroup@gmail.com>
2020-05-28 16:32:22 -0400

    think seeds is working on postgresgit add .

--- database/seeds/00_cleanup.js
+++ database/seeds/00_cleanup.js
@@ -1,3 +1,5 @@
+const knexCleaner = require("knex-cleaner");
+
 // exports.seed = async function(knex) {
 //   if (process.env.NODE_ENV != "production") {
 //     await knex("class").truncate();
@@ -5,17 +7,23 @@
 //     await knex("instructor").truncate();
 //   }
 // };
-exports.seed = async function(knex) {
-  //   switch (process.env.NODE_ENV) {
-  //     case 'production':
-  //       await knex.raw('TRUNCATE TABLE user CASCADE')
-  //       await knex.raw('TRUNCATE TABLE instructor CASCADE')
-  //       await knex.raw('TRUNCATE TABLE class CASCADE')
-  //       break
-  //     default:
-  //       // anything not using Postgres
-  //       await knex('user').truncate()
-  //       await knex('instructor').truncate()
-  //       await knex('class').truncate()
-  //   }
+// exports.seed = async function(knex) {
+//   switch (process.env.NODE_ENV) {
+//     case 'production':
+//       await knex.raw('TRUNCATE TABLE user CASCADE')
+//       await knex.raw('TRUNCATE TABLE instructor CASCADE')
+//       await knex.raw('TRUNCATE TABLE class CASCADE')
+//       break
+//     default:
+//       // anything not using Postgres
+//       await knex('user').truncate()
+//       await knex('instructor').truncate()
+//       await knex('class').truncate()
+//   }
+// };
+
+exports.seed = function(knex) {
+  return knexCleaner.clean(knex, {
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+  });
 };
